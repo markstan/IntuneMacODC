6ff9808586a18d12d73dae3871127c83864e5605
Mark Stanfill <markstan@markstan.com>
2020-08-26 17:40:00 -0500

    Initial script
    basic file collection.  Will create a IntuneMacODC.zip file in the folder it is ran from.

---
+++ IntuneMacODC.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+ 
+zip -r IntuneMacODC.zip ~/Library/Logs/Company\ Portal/*
+zip -r IntuneMacODC.zip ~/Library/Logs/Microsoft/Intune
+zip -r IntuneMacODC.zip /var/log/*
+zip -r IntunemacODC.zip /Library/Logs/Microsoft/Intune
+
+# pkg utilities
+#
+#
+
+pkgutil --pkgs > ./pkgutil_pkgs.txt
+pkgutil --pkgs | while read x; do (pkgutil --pkg-info $x; echo ""); done > ./pkgutil_info.txt
+
+zip -r IntuneMacODC.zip ./pkgutil_pkgs.txt
+zip -r IntuneMacODC.zip ./pkgutil_info.txt
