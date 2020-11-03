d563bf8743c546672488b61740fd3b47645369b9
Mark Stanfill <markstan@microsoft.com>
2020-11-03 01:01:19 -0600

    Update IntuneMacODC.sh
    Adding MDMclient output, numerous system utilities, filtered syslog

--- IntuneMacODC.sh
+++ IntuneMacODC.sh
@@ -1,5 +1,41 @@
 #! /bin/bash
- 
+
+if [ "$EUID" -ne 0 ]
+  then echo -e "Please run using 'sudo ./IntuneMacODC.sh'"
+  exit
+fi
+
+mkdir odc
+cd odc
+
+sw_vers > ./sw_vers.txt
+zip -r IntuneMacODC.zip ./sw_vers.txt
+uname -a > ./uname_a.txt
+zip -r IntuneMacODC.zip ./uname_a.txt
+
+echo -e "************************\n" >> ./profiles.txt
+echo -e "profiles status\n\n" >> ./profiles.txt
+profiles status >> ./profiles.txt
+
+echo -e "************************\n" >> ./profiles.txt
+echo -e "profiles list\n\n" >> ./profiles.txt
+profiles list >> ./profiles.txt
+
+echo -e "************************\n" >> ./profiles.txt
+echo -e "profiles show\n\n" >> ./profiles.txt
+profiles show >> ./profiles.txt
+
+echo -e "************************\n" >> ./profiles.txt
+echo -e "profiles list -verbose\n\n" >> ./profiles.txt
+profiles list -verbose >> ./profiles.txt
+
+echo -e "************************\n" >> ./profiles.txt
+echo -e "profiles show -verbose\n\n" >> ./profiles.txt
+profiles show -verbose >> ./profiles.txt
+
+zip -r IntuneMacODC.zip ./profiles.txt
+
+# Gather log directories 
 zip -r IntuneMacODC.zip ~/Library/Logs/Company\ Portal/*
 zip -r IntuneMacODC.zip ~/Library/Logs/Microsoft/Intune
 zip -r IntuneMacODC.zip /var/log/*
@@ -10,10 +46,53 @@ zip -r IntunemacODC.zip /Library/Logs/Microsoft/Intune
 #
 
 pkgutil --pkgs > ./pkgutil_pkgs.txt
-pkgutil --pkgs | while read x; do (pkgutil --pkg-info $x; echo ""); done > ./pkgutil_info.txt
+pkgutil --pkgs | while read x; do (pkgutil --pkg-info $x; echo -e ""); done > ./pkgutil_info.txt
 
 zip -r IntuneMacODC.zip ./pkgutil_pkgs.txt
 zip -r IntuneMacODC.zip ./pkgutil_info.txt
 
+log show --style syslog --info --debug --predicate 'process CONTAINS[c] "downloadd" ' --last 30d  >> ./downloadd.log
+zip -r IntuneMacODC.zip ./downloadd.log
 
+log show --style syslog --info --debug  --predicate 'process BEGINSWITH "Intune" || process CONTAINS[c] "downloadd" || process CONTAINS "mdm" ' --last 30d  >> ./intunesyslog.log
+zip -r IntuneMacODC.zip ./intunesyslog.log
+
+# System Report - double-click to open utility
+/usr/sbin/system_profiler -detailLevel full -xml > ./SystemReport.spx 2>/dev/null
+zip -r IntuneMacODC.zip ./SystemReport.spx
+
+echo -e "/usr/libexec/mdmclient QueryInstalledApps\n**************************\n\n" > ./QueryInstalledApps.txt
+/usr/libexec/mdmclient QueryInstalledApps >> ./QueryInstalledApps.txt
+zip -r IntuneMacODC.zip ./QueryInstalledApps.txt
+
+echo -e "/usr/libexec/mdmclient QueryInstalledProfiles\n**************************\n\n" > ./QueryInstalledProfiles.txt
+/usr/libexec/mdmclient QueryInstalledProfiles >> ./QueryInstalledProfiles.txt
+zip -r IntuneMacODC.zip ./QueryInstalledProfiles.txt
+
+
+echo -e "/usr/libexec/mdmclient QueryCertificates\n**************************\n\n" > ./QueryCertificates.txt
+/usr/libexec/mdmclient QueryCertificates >> ./QueryCertificates.txt
+zip -r IntuneMacODC.zip ./QueryCertificates.txt
+
+echo -e "/usr/libexec/mdmclient QueryDeviceInformation\n**************************\n\n" > ./QueryDeviceInformation.txt
+/usr/libexec/mdmclient QueryDeviceInformation >> ./QueryDeviceInformation.txt
+zip -r IntuneMacODC.zip ./QueryDeviceInformation.txt
+
+
+echo -e "/usr/libexec/mdmclient QuerySecurityInfo\n**************************\n\n" > ./QuerySecurityInfo.txt
+/usr/libexec/mdmclient QuerySecurityInfo >> ./QuerySecurityInfo.txt
+zip -r IntuneMacODC.zip ./QueryDeviceInformation.txt
+
+echo -e "/usr/libexec/mdmclient dumpSCEPVars\n**************************\n\n" > ./dumpSCEPVars.txt
+/usr/libexec/mdmclient dumpSCEPVars >> ./dumpSCEPVars.txt
+zip -r IntuneMacODC.zip ./dumpSCEPVars.txt
+
+
+
+# cleanup
+rm ./SystemReport.spx
+rm ./*.txt
+rm ./*.log
+
+# display window in Finder
 open .
